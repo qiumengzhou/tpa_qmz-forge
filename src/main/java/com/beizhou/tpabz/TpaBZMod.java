52beeab943a130d1cbfc164dca8ab4bfad5f287c
qiumengzhou <qiumengzhou@gmail.com>
2026-01-08 17:30:20 +0800

    Update TpaBZMod.java

--- src/main/java/com/beizhou/tpabz/TpaBZMod.java
+++ src/main/java/com/beizhou/tpabz/TpaBZMod.java
@@ -14,6 +14,7 @@ public class TpaBZMod {
     public static final Logger logger = LogManager.getLogger(TpaBZMod.MOD_ID);
 
     public TpaBZMod() {
+        // 我他妈的终于成功了吗
     }
 
     @SubscribeEvent
